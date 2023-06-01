f151532a71b7ba8815cd479b9ebb02a204f2969f
Simon Dutton <simonpcdutton@gmail.com>
2023-06-01 01:56:08 -0400

    helped filter out duplicate/bad restrooms

--- app/api/bathroom_routes.py
+++ app/api/bathroom_routes.py
@@ -14,7 +14,7 @@ def getBathroomsAroundLoc(lat, lng, north, east, south, west):
     # print("user is " + user.to_dict())
     if user:
         # Just going through the 26 closest bathrooms... I feel like that is enough options
-        url = f"https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=26&offset=0&lat={lat}&lng={lng}"
+        url = f"https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=100&offset=0&lat={lat}&lng={lng}"
         response = requests.get(url)
         data = response.json()
         # print(type(data))
@@ -39,6 +39,7 @@ def getBathroomsAroundLoc(lat, lng, north, east, south, west):
         # print(type(lat))
 
         i = 0
+        j = 0
 
         bathrooms_in_range = []
 
@@ -48,7 +49,7 @@ def getBathroomsAroundLoc(lat, lng, north, east, south, west):
         
         # print(type(data[0]['latitude']))
 
-        while i < 26 and i < len(data): # goes through up to 26, but limited if the results are under 26
+        while (i < 26 and i < len(data)) and (j < 100): # goes through up to 26, but limited if the results are under 26
             try:
                 
                 # print('latitude at location : ' + str(data[i]['latitude']))
@@ -82,12 +83,22 @@ def getBathroomsAroundLoc(lat, lng, north, east, south, west):
 
                 # if item is in range, add it to list
                 if lng_in_range and lat_in_range:
-                    
-                    bathrooms_in_range.append(data[i])
-
+                    # make sure it is not already in the list 
+                    for bathroom in bathrooms_in_range:
+                        if bathroom['name'] == data[i]['name'] and bathroom['latitude'] == data[i]['latitude'] and bathroom['longitude'] == data[i]['longitude']:
+                            break
+                    # couple of edge cases first: 
+                    else:
+                        # if no spaces in the street name, it is probably a fake entry
+                        if " " not in data[i]['street']:
+                            pass
+                        else:
+                            bathrooms_in_range.append(data[i])
+                            i+= 1
                 # go to the next item 
                 # print(i)
-                i+= 1
+                # print(j)
+                j+= 1
             except:
                 break # if that was the end of the breakpoint 
         # url = f"https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=10&offset=0&lat={lat}&lng={lng}"
